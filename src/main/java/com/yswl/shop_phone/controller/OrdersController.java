06837d2ae2d0c425aff30801897857b866b446e7
17379 <156495910@qq.com>
2019-09-30 10:43:16 +0800

    优化了一个牛逼的功能

--- src/main/java/com/yswl/shop_phone/controller/OrdersController.java
+++ src/main/java/com/yswl/shop_phone/controller/OrdersController.java
@@ -23,18 +23,18 @@ public class OrdersController {
         return ordersService.allOrders();
     }
     /**
-     * 分页查询已支付订单
-     */
-    @GetMapping("/orders/queryHave.do")
-    public ResultVo queryHaveOrders(Integer current,Integer size){
-        return ordersService.queryHaveOrders(current, size);
-    }
-    /**
      * 分页查询待支付订单
      */
     @GetMapping("/orders/queryNot.do")
     public ResultVo queryNotOrders(Integer current,Integer size){
         return ordersService.queryNotOrders(current,size);
+    }
+    /**
+     * 分页查询已支付订单
+     */
+    @GetMapping("/orders/queryHave.do")
+    public ResultVo queryHaveOrders(Integer current,Integer size){
+        return ordersService.queryHaveOrders(current, size);
     }
     /**
      * 分页查询已发货的订单
@@ -46,7 +46,7 @@ public class OrdersController {
     /**
      * 分页查询已完成的订单
      */
-    @GetMapping("/orders/queryPut.do")
+    @GetMapping("/orders/queryEnd.do")
     public ResultVo queryEndOrders(Integer current,Integer size){
         return ordersService.queryEndOrders(current,size);
     }
